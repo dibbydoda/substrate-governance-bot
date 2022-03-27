8a61fedd336efa3db7e154e6b662f2ea92d12534
dibbydoda <dibbydoda@gmail.com>
2022-03-27 16:08:02 +1100

    Add statemine chain.

--- chains_library.py
+++ chains_library.py
@@ -2,13 +2,14 @@
 from dataclasses import dataclass
 from disnake import Emoji
 
+
 @dataclass
 class SubstrateChain:
     """Class representing a substrate blockchain."""
     name: str
-    is_relay: bool
     endpoints: tuple
     logo_file: str
+    is_relay: bool = False
     emoji: Emoji = None
 
 
@@ -26,13 +27,18 @@ Kusama = SubstrateChain(name="Kusama",
                                    "wss://kusama-rpc.dwellir.com"),
                         logo_file="kusama-128.png")
 
-chains = (Polkadot, Kusama)
+Statemine = SubstrateChain(name="Statemine",
+                           endpoints=('wss://statemine-rpc.polkadot.io',
+                                      'wss://statemine.api.onfinality.io/public-ws',
+                                      'wss://statemine-rpc.dwellir.com'),
+                           logo_file="statemine.png")
+
+# Encointer = SubstrateChain(name="Encointer")
+
+chains = (Polkadot, Kusama, Statemine)
+
 
 def get_chain(search_name):
     for chain in chains:
         if chain.name == search_name:
             return chain
-
-
-
-
